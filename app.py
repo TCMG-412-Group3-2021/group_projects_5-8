966bd486df9c65ccaa79376110550bcf72f0bf7b
brendan-ramirez11 <b827005029@tamu.edu>
2021-10-15 13:24:06 -0500

    Factorial done.

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, json, jsonify
 import hashlib
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_and_welcome():
-    return "Howdy! Welcome to Group 3's API for TCMG 412."
+    return jsonify(input='Howdy!', output= 'Welcome to Group 3s API for TCMG 412.')
 
 @app.route('/md5/<string:helloworld>')
 def MD5(helloworld):
@@ -13,5 +13,22 @@ def MD5(helloworld):
 	hash_obj = hashlib.md5(helloworld.encode())
 	return jsonify(input=helloworld, output=hash_obj.hexdigest())
 
+@app.route('/factorial/<int:n>')
+def Factorial(n):
+	n = int(n)
+
+	fact = 1
+	if(n < 0):
+		return f"{n} is not a positive value"
+	elif(n == 0):
+		return jsonify(input=n, output=1)
+	else:
+		for i in range(1, n+1):
+			fact = fact*i
+		return jsonify(input=n, output=fact)
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+    app.run(host='0.0.0.0', port = 5000)
