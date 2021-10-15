31672c974a9084336cb7de6fc523f5a35e2e67a4
brendan-ramirez11 <b827005029@tamu.edu>
2021-10-15 15:30:45 -0500

    Code with slack message. Needs correcting for sure.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, json, jsonify
+from flask import Flask, json, jsonify, request, Response
 import hashlib
+
+
 
 app = Flask(__name__)
 
@@ -7,11 +9,11 @@ app = Flask(__name__)
 def hello_and_welcome():
     return jsonify(input='Howdy!', output= 'Welcome to Group 3s API for TCMG 412.')
 
-@app.route('/md5/<string:helloworld>')
-def MD5(helloworld):
+@app.route('/md5/<string:random>')
+def MD5(random):
 
-	hash_obj = hashlib.md5(helloworld.encode())
-	return jsonify(input=helloworld, output=hash_obj.hexdigest())
+	hash_obj = hashlib.md5(random.encode())
+	return jsonify(input=random, output=hash_obj.hexdigest())
 
 @app.route('/factorial/<int:n>')
 def Factorial(n):
@@ -27,7 +29,27 @@ def Factorial(n):
 			fact = fact*i
 		return jsonify(input=n, output=fact)
 
+@app.route('/slack-alert/<string:msg>')
+def slack(msg):
+	SLACK_URL = 'https://hooks.slack.com/services/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'
+    
+	data = { 'text': msg }
+    
+	resp = requests.post(SLACK_URL, json=data)
+	
+	if resp.status_code == 200:
+		result = True
+        mesg = "Message successfully posted to Slack channel"
+    
+	else:
+        result = False
+        mesg = "There was a problem posting to the Slack channel (HTTP response: " + str(resp.status_code) + ")."
 
+    return jsonify(
+        input=msg,
+        message=mesg,
+        output=result
+    ), 200 if resp.status_code==200 else 400
 
 if __name__ == "__main__":
     app.run(debug=True)
