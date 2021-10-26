f7929a5151b9532be3da24eef0fb05133a202d4f
naralepo <90484068+naralepo@users.noreply.github.com>
2021-10-25 19:32:54 -0500

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,24 @@
+@FLASK_APP.route("/F/<int:number>")
+
+
+def calc_fibonacci(number):
+    fibonacci = [0]
+    c1 = 0
+    c2 = 1
+    fib = 0
+    check = 0
+
+    if number < 0:
+        return jsonify(input=number, output="Error: Please enter a number greater or equal to 0")
+    elif number == 0:
+        fibonacci = [0]
+    else:
+        while check == 0:
+            fib = c1 + c2
+            c2 = c1
+            c1 = fib
+            if fib <= number:
+                fibonacci.append(fib)
+            else:
+                check = 1
+    return jsonify(input=number, output=fibonacci)
